45506612c45e4021b53c65d98859c9aa8056d641
EduCamelo <eduardocamelobs@gmail.com>
2022-10-20 18:53:07 -0300

    senacForms

---
+++ viagens/classe_viagem.py
@@ -0,0 +1,6 @@
+
+tipos_de_classe = {
+    (1, 'Economica'),
+    (2, 'executiva'),
+    (3, 'Primeira classe'),
+}
--- viagens/forms.py
+++ viagens/forms.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 from django import forms
 from tempus_dominus.widgets import DatePicker
-
+from datetime import datetime
+from viagens.classe_viagem import tipos_de_classe
 class ViagemForms(forms.Form):
 
     origem = forms.CharField(label='Origem', max_length= 100)
     destino = forms.CharField(label='Destino', max_length= 100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data de pesquisa', disabled=True, initial=datetime.today)
+    classe_viagem = forms.ChoiceField(label='Opção de voo', choices= tipos_de_classe)
